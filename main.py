94031be45b3d196f331f718ea972781984169909
jen-adolfo29 <jenadolfo5@gmail.com>
2023-08-10 14:15:06 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# email slicer
+
+def main():
+    print("This is an Email Slicer")
+    print("")
+
+    email_input = input("Input your email address: ")
+
+    (username, domain) = email_input.split("@")
+    (domain, extension) = domain.split(".")
+
+    print("Username: ", username)
+    print("Domain: ", domain)
+    print("Extension: ", extension)
+while True:
+    main()
